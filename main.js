9d6ed072d8f55a6d2efd1d738188955037d36f50
Sajawal Fareedi <sajawalfareedi448@gmail.com>
2023-08-18 12:00:35 +0500

    Finalized the development

--- main.js
+++ main.js
@@ -18,19 +18,21 @@ const GOPLUS_MIN_VP = 80; // 80%
 const GOPLUS_MIN_SR = 85; // 85%
 
 const MAX_GAS_FEES = 30 * (10 ** 9); // 30 Gwei
-const EXTRA_GAS_FEES = 0 // 3 * (10 ** 9); // 3 Gwei  // TODO
-const GAS_LIMIT = 500000
+const EXTRA_GAS_FEES = 3 * (10 ** 9); // 3 Gwei
+const GAS_LIMIT = 500000;
+const APPROVE_GAS_LIMIT = 150000;
 
 const DEADLINE = 10; // 10 minutes
 const MAX_TAX = 10; // 10%
-const BUDGET = 10; // 50 USD  // TODO
+const BUDGET = 50; // 50 USD
 
-const MIN_USD_BALANCE = 70;
-const MIN_USD_BALANCE_SELL_GAS = 15
-const MIN_PROFIT = 1; // 50% // TODO
+const MIN_USD_BALANCE = 90;
+const MIN_USD_BALANCE_SELL_GAS = 25;
+const MIN_PROFIT = 0.5; // 50%
 
 const APPROVED_TOKEN_AMOUNT = '115792089237316195423570985008687907853269984665640564039457584007913129639935'; // (2^256 - 1 )
 
+const MAX_HOLDER_PERCENT = 0.02; // 2%
 const ELIGBL_HOLDERS = [
     "0x0000000000000000000000000000000000000000",
     "0x000000000000000000000000000000000000dead",
@@ -38,6 +40,7 @@ const ELIGBL_HOLDERS = [
 ];
 
 let BUYING_ENABLED = true;
+let ONLY_ONE = false;
 
 const abiCoder = ethers.utils.defaultAbiCoder;
 const etherScanProvider = new ethers.providers.EtherscanProvider({ name: "homestead", chainId: 1 }, "VI19J433TAWE9DCDFI5J1FENQQDU6TW35X");
@@ -144,7 +147,7 @@ const doGoPlusScan = async (token) => {
 
                 } else if (["owner_percent", "creator_percent"].includes(_key)) {
 
-                    if (parseFloat(data[_key]) <= 0.05) {
+                    if (parseFloat(data[_key]) <= MAX_HOLDER_PERCENT) {
                         vip_points += 1
                     }
 
@@ -241,7 +244,7 @@ const doIsHoneyPotScan = async (token) => {
 
 const isTokenSafe = async (token) => {
     try {
-        log("Sniper", "Scanning Started");
+        // log("Sniper", "Scanning Started");
 
         const isHoneyPotResult = await doIsHoneyPotScan(token);
         const goPlusResult = await doGoPlusScan(token);
@@ -264,7 +267,7 @@ const isTokenSafe = async (token) => {
                     const data = res.data.result[token];
 
                     if (ELIGBL_HOLDERS.includes(data.owner_address)) {
-                        if (parseFloat(data.creator_percent) <= 0.05) {
+                        if (parseFloat(data.creator_percent) <= MAX_HOLDER_PERCENT) {
                             if (data.lp_holders) {
                                 let totalHoldings = 0;
 
@@ -313,7 +316,7 @@ const approve = async (token, tokenContract) => {
             to: token,
             from: wallet.address,
             nonce: nonce,
-            gasLimit: 150000,
+            gasLimit: APPROVE_GAS_LIMIT,
             gasPrice: gasPrice,
             data: tokenContract.methods.approve(ROUTER_ADDR, APPROVED_TOKEN_AMOUNT).encodeABI(),
         }
@@ -451,9 +454,10 @@ const createBuyTxAndSend = async (token, gas, maxBuyAmountInETH) => {
 const sellCronStart = async (tx, token, buyingPrice, pairAddr, decimals) => {
     try {
 
+        let receipt;
         try {
 
-            const receipt = await tx.wait();
+            receipt = await tx.wait();
             if (receipt.status == 0) {
                 log("Sniper", `Buy Transaction Failed - Token: ${token} - Tx: https://etherscan.io/tx/${receipt.hash}`);
                 return;
@@ -475,36 +479,49 @@ const sellCronStart = async (tx, token, buyingPrice, pairAddr, decimals) => {
 
         await approve(token, tokenContract);
 
-        const ATTS = 1;
-        let startTokenBalance = 0
+        let ATTS = 1;
+        // let startTokenBalance = 0
 
         while (true) {
             const ethPrice = await etherScanProvider.getEtherPrice();
             const reserves = await pairContract.methods.getReserves().call();
-            const currentPrice = (Number(reserves._reserve1) / Number(reserves._reserve0)) * ethPrice;
-            /* 
-            * TODO: Check amount if WETH available in reserve. 
-            * If less than 0.5 WETH then the liquidity has been taken, 
-            * then break the loop!
-            */
+
+            const reserve0 = Number(reserves._reserve0);
+            const reserve1 = Number(reserves._reserve1);
+
+            let tokenReserve;
+            let wethReserve;
+
+            if (decimals < 18) {
+                // decimals < 18 ? reserve1 / (10 ** decimals) : reserve0 / (10 ** decimals);
+                tokenReserve = reserve0 < reserve1 ? reserve0 / (10 ** decimals) : reserve1 / (10 ** decimals);
+                // decimals < 18 ? reserve0 / (10 ** 18) : reserve1 / (10 ** 18);
+                wethReserve = reserve0 < reserve1 ? reserve1 / (10 ** 18) : reserve0 / (10 ** 18);
+            } else {
+                tokenReserve = reserve0 < reserve1 ? reserve1 / (10 ** decimals) : reserve0 / (10 ** decimals);
+                wethReserve = reserve0 < reserve1 ? reserve0 / (10 ** 18) : reserve1 / (10 ** 18);
+            }
+            
+            const currentPrice = (wethReserve / tokenReserve) * ethPrice;
+
+            if (wethReserve < 0.1) {
+                break;
+            };
+
             const priceDifferenceInPercentage = ((currentPrice - parseFloat(buyingPrice)) / parseFloat(buyingPrice)) * 100;
 
             if (priceDifferenceInPercentage >= ((MIN_PROFIT * 100) * ATTS)) {
                 let tokenBalance = await tokenContract.methods.balanceOf(wallet.address).call();
                 tokenBalance = parseInt((tokenBalance).toString()) / (10 ** decimals);
 
-                if (ATTS == 1) {
-                    startTokenBalance = tokenBalance;
-                }
+                // if (ATTS == 1) {
+                //     startTokenBalance = tokenBalance;
+                // }
 
-                const currentTokenBalancePercentage = Math.round((tokenBalance / startTokenBalance) * 100);
+                // const currentTokenBalancePercentage = Math.round((tokenBalance / startTokenBalance) * 100);
+                const tokenBalanceWorth = tokenBalance * currentPrice;
 
-                if (currentTokenBalancePercentage <= 5) {
-                    /* 
-                    * TODO: Not a good idea to check the % left of token. 
-                    * Because 5% can worth more than expected. Check the $ worth instead.
-                    * If less than $10, then break.
-                    */
+                if (tokenBalanceWorth <= 5) {
                     break;
                 }
 
@@ -522,18 +539,13 @@ const sellCronStart = async (tx, token, buyingPrice, pairAddr, decimals) => {
                         amountOutMinWithSlippage,
                         path,
                         wallet.address,
-                        /* 
-                        * TODO: I guess 30 mins wait if fine, but then tx will be in pending status
-                        * for 30 mins, and other txs won't make it through. Use a small waiting time
-                        * like 5 or 10 minutes.
-                        */
                         ((new Date().getTime() / 1000) + (DEADLINE * 60)).toFixed(0)
                     ]
                 );
 
                 const input = `0x791ac947${data.slice(2)}`;
                 const nonce = await provider.getTransactionCount(wallet.address);
-                const gasPrice = parseInt((await provider.getGasPrice()).toString());
+                const gasPrice = parseInt((await provider.getGasPrice()).toString()) + EXTRA_GAS_FEES;
                 const finalTx = {};
 
                 finalTx["from"] = wallet.address;
@@ -567,6 +579,7 @@ const sellCronStart = async (tx, token, buyingPrice, pairAddr, decimals) => {
                                 const input = prompt("Not able to sell the token! Please sell manually and type Y: ");
 
                                 if (input == "Y" || input == "y") {
+                                    ATTS += 1;
                                     break;
                                 };
                             };
@@ -580,13 +593,22 @@ const sellCronStart = async (tx, token, buyingPrice, pairAddr, decimals) => {
                             const input = prompt("Not able to sell the token! Please sell manually and type Y: ");
 
                             if (input == "Y" || input == "y") {
+                                ATTS += 1;
                                 break;
                             };
                         };
                     };
 
                 } else {
-                    log("Sniper", `Balance is low! Can't Sellllll!`);
+                    log("Sniper", `======================================== Balance is low! Can't Sellllll! ==================================`);
+
+                    while (true) {
+                        const input = prompt("Please put some ETH manually and type Y: ");
+
+                        if (input == "Y" || input == "y") {
+                            break;
+                        };
+                    };
                 };
             };
         };
@@ -612,7 +634,8 @@ const sellCronStart = async (tx, token, buyingPrice, pairAddr, decimals) => {
 };
 
 const handleNewToken = async (token0, token1, txHash, pairAddr) => {
-    if (BUYING_ENABLED) {
+    if (BUYING_ENABLED && !ONLY_ONE) {
+        // ONLY_ONE = true;
         const { data } = await provider.getTransaction(txHash);
 
         if (creationMethods.includes(data)) {
@@ -626,7 +649,7 @@ const handleNewToken = async (token0, token1, txHash, pairAddr) => {
             const time = new Date().toLocaleString();
 
             if (success) {
-                console.log(`[${time}] [${token}] [${success}]`, data);
+                console.log(`[${time}] [https://dexscreener.com/ethereum/${token}] [${success}]`, data);
 
                 const result = await createBuyTxAndSend(token, data.honeypot.buyGas, data.honeypot.maxBuy);
                 if (result.success) {
@@ -645,13 +668,19 @@ const handleNewToken = async (token0, token1, txHash, pairAddr) => {
     log("Sniper", "Listenings to Events");
     log("", "");
 
+    // const pairAddr = "0x7ef0dC73D9807C491C7A34f502f3af8626322f9B".toLowerCase();
+    // const token = "0xeAf20a9C529F0dA7943558e66a46A684f7A64245".toLowerCase();
+
+    // const pairAddr = "0x931CCb5B70fe54BeBF36b4d5BB80D17B2c7f80D2".toLowerCase();
+    // const token = "0x1441ba039f71a6b942a77829d0d1784fb9b771bc".toLowerCase();
+
     // const pairAddr = "0x606EEf5677C104bEaE943bb2F9758113e06f34dB".toLowerCase();
     // const token = "0x175fE43259fBC8F0Ae3e3E7E70cCd53e292706FC".toLowerCase();
-    // await sellCronStart("", token, 0.015, pairAddr, 18);
+    // await sellCronStart("", token, 0.0000015, pairAddr, 9);
 
     const events = factoryContract.events.allEvents();
     events.on('data', (event) => {
         handleNewToken(event.returnValues.token0, event.returnValues.token1, event.transactionHash, event.returnValues.pair);
     });
 
-});
+})();
